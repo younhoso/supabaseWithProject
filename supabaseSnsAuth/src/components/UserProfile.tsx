94a2bc0f85f8e20fa3418ee7848659624c425f25
younhoso <thdbsgh3443@kakao.com>
2025-04-05 13:53:11 +0900

    Feat: hooks/useAuth 사용

--- supabaseSnsAuth/src/components/UserProfile.tsx
+++ supabaseSnsAuth/src/components/UserProfile.tsx
@@ -1,43 +1,9 @@
 'use client';
 
-import { useEffect, useState } from 'react';
-
-import { User } from '@/types/user';
-import { supabase } from '@/utils/supabase/supabaseClient';
+import useAuth from '@/hooks/useAuth';
 
 export default function UserProfile() {
-  const [users, setUsers] = useState<User | null>(null);
-
-  // Supabase 세션 가져오기
-  useEffect(() => {
-    const fetchUser = async () => {
-      // 현재 세션을 먼저 확인
-      const {
-        data: { session },
-      } = await supabase.auth.getSession();
-      if (session?.user) {
-        // 세션이 있으면 사용자 정보 가져오기
-        setUsers(session.user as User);
-      }
-    };
-
-    fetchUser();
-
-    // 세션 상태 변화 리스너 설정
-    const { data: authListener } = supabase.auth.onAuthStateChange((_, session) => {
-      setUsers(session?.user as User);
-    });
-
-    return () => {
-      authListener?.subscription.unsubscribe(); // 컴포넌트 언마운트 시 리스너 제거
-    };
-  }, []);
-
-  // 로그아웃 처리
-  const handleLogout = async () => {
-    await supabase.auth.signOut();
-    setUsers(null); // 로그아웃 후 유저 상태를 null로 설정
-  };
+  const { users, handleLogout } = useAuth();
 
   return (
     <div>
@@ -45,11 +11,11 @@ export default function UserProfile() {
         <>
           <div>
             <div>
-              <img src={users.identities[0].identity_data.avatar_url} alt="사용자" />
+              <img src={users?.identities?.[0]?.identity_data?.avatar_url} alt="사용자" />
               <div>사용자</div>
             </div>
             <div>
-              <p>{users.identities[0].identity_data.name}</p>
+              <p>{users.identities?.[0]?.identity_data?.name}</p>
               <p>{users?.email}</p>
             </div>
             <div onClick={handleLogout}>로그아웃</div>
